08e0a5ca0de301191f18f163779a45cbedaea3ad
PetronilahKwamboka <pettykwamboka@gmail.com>
2020-05-15 15:55:54 +0300

    add code of order prototype totalPrice

--- js/scripts.js
+++ js/scripts.js
@@ -6,30 +6,28 @@ function Order(size, crust, topping, quantity, delivery) {
   this.quantity = quantity;
   this.delivery = delivery;
 }
+
 Order.prototype.totalPrice = function(){
-  var cartTotalprice = 0;
-  for(var arrayElement = 0; arrayElement< totalPriceArray.length; arrayElement ++){
-    cartTotalprice += totalPriceArray[arrayElement];
-  }
-  return cartTotalprice;
+  return (this.pizzaSize + this.topping) * this.quantity;
 }
+
 
 //user interface logic
 
 $(document).ready(function(){
-  $("form#btnsubmit").submit(function(event){
+  $("form#new-pizza").submit(function(event){
     event.preventDefault();
-      var pizzaSizes = parseInt($("input[type=checkbox][name=pizza-size]:checked").val());
+      var pizzaSize = parseInt($("input[type=checkbox][name=pizza-size]:checked").val());
       var crusts = parseInt($("input[type=checkbox][name=crust]:checked").val());
       var toppings = parseInt($("input[type=checkbox][name=topping]:checked").val());
       var quantity = parseInt($("#quantity").val());
       var delivery = parseInt($("#delivery").val());
 
 
-      var newOrder = new Order(pizzaSizes, crusts, toppings, quantity, delivery);
+      var newOrder = new Order(pizzaSize, crusts, toppings, quantity, delivery);
 
 
-    $("ul#orders").append("<li><span class='list-orders'>" + newOrder.pizzaSize + totalPrice() + "</span></li> ");
+    $("ul#lists").append("<li><span class='list-orders'>" + newOrder.totalPrice() + "</span></li> ");
 
     $("input[type=checkbox][name=pizza-size]:checked").val("");
     $("input[type=checkbox][name=crust]:checked").val("");
