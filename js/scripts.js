0affad15ac67f9874d1e954fed806f07b7c26982
PetronilahKwamboka <pettykwamboka@gmail.com>
2020-05-16 22:03:35 +0300

    add resetFields function

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,21 @@ function Order(size, crust, topping, quantity, delivery) {
 }
 
 Order.prototype.totalPrice = function(){
-  return (this.pizzaSize + this.topping) * this.quantity;
+  return (this.pizzaSize + this.crust + this.topping) * this.quantity;
 }
 
 Order.prototype.fullOrder = function(){
   return this.pizzaSize + ", " + this.crust + ", " + this.topping + "," + this.quantity + "," + this.delivery;
 }
 
+function resetFields() {
 
+  $("input[type=checkbox][name=pizza-size]:checked").val("").prop('checked', false);
+  $("input[type=checkbox][name=crust]:checked").val("").prop('checked', false);
+  $("input[type=checkbox][name=topping]:checked").val("").prop('checked', false);
+  $("#quantity").val("");
+  $("#delivery").val("");
+}
 //user interface logic
 
 $(document).ready(function(){
@@ -26,23 +33,21 @@ $(document).ready(function(){
       var toppings = parseInt($("input[type=checkbox][name=topping]:checked").val());
       var quantity = parseInt($("#quantity").val());
       var delivery = $("#delivery").val();
-      if(delivery.selected === "Deliver"){
-        alert('Enter location:');
+      if($("#delivery").val() === "Deliver"){
+        alert('Delivery is 200/=');
+        alert(prompt('Enter your location'));
+        alert('Your order will be delivered to your location');
       } else {
-        alert("Your order is ready for pick-up");
+        alert("You can pick up your order before closing hours");
       }
 
 
       var newOrder = new Order(pizzaSize, crusts, toppings, quantity, delivery);
 
     $(".display-content").show();
-    $("ul#lists").append("<li><span class='list-orders'>" + newOrder.fullOrder() + ("  ||") +  ("Total is: ") + newOrder.totalPrice() + "</span></li> ");
+    $("ul#lists").append("<li><span class='list-orders'>" + newOrder.fullOrder() + ("  ||") +  ("Total is Ksh: ") + newOrder.totalPrice() + "</span></li> ");
 
-    $("input[type=checkbox][name=pizza-size]:checked").val("").prop('checked', false);
-    $("input[type=checkbox][name=crust]:checked").val("").prop('checked', false);
-    $("input[type=checkbox][name=topping]:checked").val("").prop('checked', false);
-    $("#quantity").val("");
-    $("#delivery").val("");
+    resetFields();
 
   });
 });
